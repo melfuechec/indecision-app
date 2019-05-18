b0e6712f5c3354082308a99c9a2899268fbd98d5
melfuechec <melular@gmail.com>
2019-05-18 14:29:47 -0500

    added cors require to server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-import cors from 'cors';
+const cors = require('cors');
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));

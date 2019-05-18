98285b5f34f6dfd65ef14afc3661e35705edaaa5
melfuechec <melular@gmail.com>
2019-05-18 14:06:14 -0500

    Added Procfile and cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 // send the user to index html page inspite of the url
-app.get('*', (req, res) => {
+app.get('*', cors(), (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 

a6311308c169df0625c93bf4767d9335a7c74458
melfuechec <melular@gmail.com>
2019-05-18 15:55:44 -0500

    heroku config:set NPM_CONFIG_PRODUCTION=false

--- server.js
+++ server.js
@@ -2,37 +2,36 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-const cors = require('cors');
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 
 // send the user to index html page inspite of the url
-app.get('*', cors(), (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+  console.log('server started')
 });
 
 app.listen(port);
-console.log('server started')
 
-const http = require('http');
+// const http = require('http');
 
-process
-  .on('SIGTERM', shutdown('SIGTERM'))
-  .on('SIGINT', shutdown('SIGINT'))
-  .on('uncaughtException', shutdown('uncaughtException'));
+// process
+//   .on('SIGTERM', shutdown('SIGTERM'))
+//   .on('SIGINT', shutdown('SIGINT'))
+//   .on('uncaughtException', shutdown('uncaughtException'));
 
-setInterval(console.log.bind(console, 'tick'), 1000);
-http.createServer((req, res) => res.end('hi'))
-  .listen(process.env.PORT || 3000, () => console.log('Listening'));
+// setInterval(console.log.bind(console, 'tick'), 1000);
+// http.createServer((req, res) => res.end('hi'))
+//   .listen(process.env.PORT || 3000, () => console.log('Listening'));
 
-function shutdown(signal) {
-  return (err) => {
-    console.log(`${ signal }...`);
-    if (err) console.error(err.stack || err);
-    setTimeout(() => {
-      console.log('...waited 5s, exiting.');
-      process.exit(err ? 1 : 0);
-    }, 5000).unref();
-  };
-}
+// function shutdown(signal) {
+//   return (err) => {
+//     console.log(`${ signal }...`);
+//     if (err) console.error(err.stack || err);
+//     setTimeout(() => {
+//       console.log('...waited 5s, exiting.');
+//       process.exit(err ? 1 : 0);
+//     }, 5000).unref();
+//   };
+// }
